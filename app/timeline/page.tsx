6b50c27acac989bd30534774444d183f4a39f532
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:02:57 -0700

    Add magazine timeline page

---
+++ app/timeline/page.tsx
@@ -0,0 +1,64 @@
+export default function Timeline(){
+
+return(
+<div className="slideBackground">
+
+<h1 className="slideTitle">
+Magazine Timeline
+</h1>
+
+
+<div className="timeline">
+
+
+<div className="event">
+<h2>01</h2>
+<h3>July 31</h3>
+<p>
+Topics assigned,
+<br/>
+website up
+</p>
+</div>
+
+
+<div className="event">
+<h2>02</h2>
+<h3>Aug. 30</h3>
+<p>
+Rough drafts
+<br/>
+finished
+</p>
+</div>
+
+
+<div className="event">
+<h2>03</h2>
+<h3>Sept. 10</h3>
+<p>
+Copy edits done,
+<br/>
+layout finalized
+</p>
+</div>
+
+
+<div className="event">
+<h2>04</h2>
+<h3>Sept. 15</h3>
+<p>
+PUBLICATION
+<br/>
+DAY
+</p>
+</div>
+
+
+</div>
+
+
+</div>
+)
+
+}
