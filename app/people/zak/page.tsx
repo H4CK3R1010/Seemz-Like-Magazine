ae19569beba9564565ad6b242962f3f314134801
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:27:18 -0700

    Create zak page.tsx

---
+++ app/people/zak/page.tsx
@@ -0,0 +1,17 @@
+export default function Zak(){
+
+return(
+<div className="background">
+
+<h1 className="title">
+Hi I'm Zak
+</h1>
+
+<h2 className="subtitle">
+Website Manager, Author
+</h2>
+
+</div>
+)
+
+}
