068f9c3b69e4befecc5cd777d33aaab4106f206c
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:03:23 -0700

    Add pricing page

---
+++ app/pricing/page.tsx
@@ -0,0 +1,55 @@
+export default function Pricing(){
+
+return(
+
+<div className="slideBackground">
+
+
+<h1 className="slideTitle">
+Pricing
+</h1>
+
+
+<div className="pricing">
+
+
+<h2>
+First Year (4 issues) Free
+</h2>
+
+<p>
+Cost to print: who knows
+</p>
+
+
+
+<h2>
+Once we gain loyal followers...
+</h2>
+
+<p>
+Membership: $12 per year ($3 per issue)
+<br/>
+Single issue: $4 per issue
+</p>
+
+
+
+<h2>
+4 issues a year
+</h2>
+
+<p>
+One magazine for every season
+</p>
+
+
+
+</div>
+
+
+</div>
+
+)
+
+}
