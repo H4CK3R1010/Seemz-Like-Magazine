b031c33bbd1bae08fa4bd2b714cb66c215a07201
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:26:17 -0700

    Create sophianpage.tsx

---
+++ app/people/sophia/page.tsx
@@ -0,0 +1,17 @@
+export default function Sophia(){
+
+return(
+<div className="background">
+
+<h1 className="title">
+Hi I'm Sophia
+</h1>
+
+<h2 className="subtitle">
+Copy editor, Formatter, Author
+</h2>
+
+</div>
+)
+
+}
