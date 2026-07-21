95dc80eb7c41630940f10b164f1639cc4d76f140
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:28:01 -0700

    Create zoe page.tsx

---
+++ app/people/zoe/page.tsx
@@ -0,0 +1,17 @@
+export default function Zoe(){
+
+return(
+<div className="background">
+
+<h1 className="title">
+Hi I'm Zoe
+</h1>
+
+<h2 className="subtitle">
+Editor-in-chief, Social Media Manager, Formatter, Author
+</h2>
+
+</div>
+)
+
+}
