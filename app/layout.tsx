1fdb392a45c133f68518bd9375498c0d327384e8
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:06:29 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,72 @@
+import Link from "next/link";
 import "./globals.css";
 
+
 export const metadata = {
-  title: "Seemz Like Magazine",
-  description: "A magazine created by cousins",
+
+  title: "Seemz Like...",
+
+  description: "For kids, By kids"
+
 };
 
+
+
 export default function RootLayout({
+
   children,
+
 }: {
+
   children: React.ReactNode;
+
 }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+
+
+return (
+
+<html lang="en">
+
+<body>
+
+
+<nav>
+
+<Link href="/">
+Home
+</Link>
+
+
+<Link href="/timeline">
+Timeline
+</Link>
+
+
+<Link href="/pricing">
+Pricing
+</Link>
+
+
+<Link href="/topics">
+Topics
+</Link>
+
+
+<Link href="/goals">
+Goals
+</Link>
+
+
+</nav>
+
+
+{children}
+
+
+</body>
+
+</html>
+
+);
+
 }
