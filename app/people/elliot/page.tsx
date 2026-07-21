3e1c1a5572ddd36cf6333882e2e120a506719d2d
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:17:22 -0700

    Create elliot page.tsx

---
+++ app/people/elliot/page.tsx
@@ -0,0 +1,17 @@
+export default function Elliot(){
+
+return(
+<div className="background">
+
+<h1 className="title">
+Hi I'm Elliot
+</h1>
+
+<h2 className="subtitle">
+Illustrator, Author
+</h2>
+
+</div>
+)
+
+}
