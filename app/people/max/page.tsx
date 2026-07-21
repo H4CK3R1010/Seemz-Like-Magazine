1e1431e929ab486df48665343e1d6d91bb78d3a8
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:25:29 -0700

    Create max page.tsx

---
+++ app/people/max/page.tsx
@@ -0,0 +1,17 @@
+export default function Max(){
+
+return(
+<div className="background">
+
+<h1 className="title">
+Hi I'm Max
+</h1>
+
+<h2 className="subtitle">
+Digital Artist, Author
+</h2>
+
+</div>
+)
+
+}
