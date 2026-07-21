6f34ef223ccfdb687f672a2d245fe3850e2f563a
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:04:24 -0700

    Add goals page

---
+++ app/goals/page.tsx
@@ -0,0 +1,70 @@
+export default function Goals(){
+
+return(
+
+<div className="background">
+
+
+<h1 className="slideTitle">
+Goals
+</h1>
+
+
+<div className="goals">
+
+
+<h2>
+Year 1
+</h2>
+
+<p>
+Publish 4 issues, reach 250 subscribers and 250 followers
+</p>
+
+
+
+<h2>
+Year 2
+</h2>
+
+<p>
+Build an active community
+<br/>
+500 subscribers
+<br/>
+Social Media: 500 followers
+</p>
+
+
+
+<h2>
+Year 3
+</h2>
+
+<p>
+A steady number of subscribers and followers,
+<br/>
+partnerships with local organizations
+</p>
+
+
+
+<h2>
+Year 4
+</h2>
+
+<p>
+A recognizable brand, recruit other kids,
+<br/>
+add special edition issues
+</p>
+
+
+</div>
+
+
+</div>
+
+)
+
+}
