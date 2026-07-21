00c5b274b7026c1ce82ca81dd955465c0ec992ec
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:24:59 -0700

    Create maddox page.tsx

---
+++ app/people/maddox/page.tsx
@@ -0,0 +1,17 @@
+export default function Maddox(){
+
+return(
+<div className="background">
+
+<h1 className="title">
+Hi I'm Maddox
+</h1>
+
+<h2 className="subtitle">
+Author
+</h2>
+
+</div>
+)
+
+}
