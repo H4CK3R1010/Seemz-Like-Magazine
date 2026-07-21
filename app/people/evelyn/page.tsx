c393875d6488f55992aaccbd134f2c17978d7d53
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-21 15:24:26 -0700

    Create evelyn page.tsx

---
+++ app/people/evelyn/page.tsx
@@ -0,0 +1,17 @@
+export default function Evelyn(){
+
+return(
+<div className="background">
+
+<h1 className="title">
+Hi I'm Evelyn
+</h1>
+
+<h2 className="subtitle">
+Graphic Designer, Author
+</h2>
+
+</div>
+)
+
+}
