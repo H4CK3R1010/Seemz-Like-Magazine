578bf2fc3ba1cdcd7fe2be86bdc8ffaa1083b7fc
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-01 13:43:40 -0700

    Update Sophia page design

--- app/people/sophia/page.tsx
+++ app/people/sophia/page.tsx
@@ -1,17 +1,23 @@
 export default function Sophia(){
 
 return(
-<div className="background">
 
-<h1 className="title">
+<div className="peopleBackground">
+
+<div className="peopleBox">
+
+<h1>
 Hi I'm Sophia
 </h1>
 
-<h2 className="subtitle">
-Copy editor, Formatter, Author
-</h2>
+<p>
+Coming soon...
+</p>
 
 </div>
+
+</div>
+
 )
 
 }
