8547720cd391cbca5c089ef5cdcf1de523122520
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-01 13:28:01 -0700

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,22 +3,16 @@ export default function Contact(){
 return(
 <div className="background">
 
-<div className="pageBox">
-
-<h1>
+<div className="title">
 Contact Us
-</h1>
-
-<p>
-You can contact us at{" "}
-<a href="mailto:seemzlike@gmail.com">
-seemzlike@gmail.com
-</a>
-</p>
-
 </div>
 
+<div className="subtitle">
 </div>
+You can contact us at seemzlike@gmail.com!
+</div>
+
+
 )
 
 }
