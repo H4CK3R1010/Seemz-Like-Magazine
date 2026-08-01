af96feca1b0379b10ad519020a460d1167bd9a1e
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-01 13:13:33 -0700

    Add Pricing page

---
+++ app/pricing/page.tsx
@@ -0,0 +1,19 @@
+export default function Pricing(){
+
+return(
+
+<div className="background">
+
+<div className="title">
+Pricing
+</div>
+
+<div className="subtitle">
+Coming soon...
+</div>
+
+</div>
+
+)
+
+}
