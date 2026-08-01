e80c0905fbdc0d28fa40b5be450a305d91597347
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-31 21:04:39 -0700

    Simplify homepage design

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,102 @@
 import Link from "next/link";
 
-export default function Home() {
 
-  const people = [
-    "zoe",
-    "maddox",
-    "sophia",
-    "evan",
-    "zak",
-    "evelyn",
-    "max",
-    "elliot"
-  ];
+export default function Home(){
 
-  return (
-    <div className="background">
 
-      <div style={{ paddingTop: "80px", textAlign: "center" }}>
-        <div className="textBox">
-          <h1 className="title">Seemz Like...</h1>
-          <p className="subtitle">For kids, By kids</p>
-        </div>
-      </div>
+const people=[
 
-      <div style={{ textAlign: "center", marginTop: "120px" }}>
+"zoe",
 
-        <div className="textBox" style={{ marginBottom: "35px" }}>
-          <h2>Meet the Team</h2>
-        </div>
+"maddox",
 
-        <br />
+"sophia",
 
-        {people.map((person) => (
-          <Link
-            key={person}
-            href={`/people/${person}`}
-            className="card"
-          >
-            {person.charAt(0).toUpperCase() + person.slice(1)}
-          </Link>
-        ))}
+"evan",
 
-      </div>
+"zak",
 
-      <div
-        style={{
-          textAlign: "center",
-          marginTop: "120px",
-          paddingBottom: "80px",
-        }}
-      >
+"evelyn",
 
-        <div className="textBox" style={{ marginBottom: "35px" }}>
-          <h2>Explore the Magazine</h2>
-        </div>
+"max",
 
-        <br />
+"elliot"
 
-        <Link className="card" href="/timeline">
-          Timeline
-        </Link>
+];
 
-        <Link className="card" href="/pricing">
-          Pricing
-        </Link>
 
-        <Link className="card" href="/goals">
-          Goals
-        </Link>
+return(
 
-      </div>
+<div className="background">
 
-    </div>
-  );
+
+<div style={{
+textAlign:"center",
+paddingTop:"120px"
+}}>
+
+
+<h1 className="title">
+
+Seemz Like...
+
+</h1>
+
+
+<h2 className="subtitle">
+
+For kids, By kids
+
+</h2>
+
+
+</div>
+
+
+
+<div style={{
+textAlign:"center",
+marginTop:"120px"
+}}>
+
+
+<h2>
+
+Meet the Team
+
+</h2>
+
+
+
+{
+
+people.map((person)=>(
+
+<Link
+
+key={person}
+
+href={`/people/${person}`}
+
+className="card"
+
+>
+
+{person.charAt(0).toUpperCase()+person.slice(1)}
+
+</Link>
+
+
+))
+
+}
+
+
+</div>
+
+
+</div>
+
+)
+
 }
