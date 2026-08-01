12be336b8ef0ee5c7e41eaad6a4854c59707b2c5
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-01 13:14:05 -0700

    Add Donate page

---
+++ app/donate/page.tsx
@@ -0,0 +1,19 @@
+export default function Donate(){
+
+return(
+
+<div className="background">
+
+<div className="title">
+Donate
+</div>
+
+<div className="subtitle">
+Coming soon...
+</div>
+
+</div>
+
+)
+
+}
