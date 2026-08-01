57817049d4893e3362d232556ab5f293621f078c
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-01 13:13:02 -0700

    Add About page

---
+++ app/about/page.tsx
@@ -0,0 +1,19 @@
+export default function About(){
+
+return(
+
+<div className="background">
+
+<div className="title">
+About
+</div>
+
+<div className="subtitle">
+Coming soon...
+</div>
+
+</div>
+
+)
+
+}
