d96d7fb07c095af12df6b5d1570d9f411c84c2a1
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-01 13:14:55 -0700

    Add Contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,19 @@
+export default function Contact(){
+
+return(
+
+<div className="background">
+
+<div className="title">
+Contact
+</div>
+
+<div className="subtitle">
+Coming soon...
+</div>
+
+</div>
+
+)
+
+}
