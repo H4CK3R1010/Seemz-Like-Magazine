2bdba6c7089924af2185204b45151d5a8eb212bf
H4CK3R1010 <zakarymyles@gmail.com>
2026-07-31 20:27:06 -0700

    Improve homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,75 @@
 import Link from "next/link";
 
-export default function Home(){
+export default function Home() {
 
-const people=[
-"zoe",
-"maddox",
-"sophia",
-"evan",
-"zak",
-"evelyn",
-"max",
-"elliot"
-];
+  const people = [
+    "zoe",
+    "maddox",
+    "sophia",
+    "evan",
+    "zak",
+    "evelyn",
+    "max",
+    "elliot"
+  ];
 
+  return (
+    <div className="background">
 
-return(
-<div className="background">
+      <div style={{ paddingTop: "80px", textAlign: "center" }}>
+        <div className="textBox">
+          <h1 className="title">Seemz Like...</h1>
+          <p className="subtitle">For kids, By kids</p>
+        </div>
+      </div>
 
-<div className="title">
-Seemz Like...
-</div>
+      <div style={{ textAlign: "center", marginTop: "120px" }}>
 
-<div className="subtitle">
-For kids, By kids
-</div>
+        <div className="textBox" style={{ marginBottom: "35px" }}>
+          <h2>Meet the Team</h2>
+        </div>
 
+        <br />
 
-<div style={{textAlign:"center", marginTop:200}}>
+        {people.map((person) => (
+          <Link
+            key={person}
+            href={`/people/${person}`}
+            className="card"
+          >
+            {person.charAt(0).toUpperCase() + person.slice(1)}
+          </Link>
+        ))}
 
-{people.map((person)=>(
-<Link
-className="card"
-href={`/people/${person}`}
-key={person}
->
-{person}
-</Link>
-))}
+      </div>
 
-</div>
+      <div style={{ textAlign: "center", marginTop: "120px", paddingBottom: "80px" }}>
 
+        <div className="textBox" style={{ marginBottom: "35px" }}>
+          <h2>Explore the Magazine</h2>
+        </div>
 
-<div style={{textAlign:"center",marginTop:100}}>
+        <br />
 
-<Link className="card" href="/timeline">
-Timeline
-</Link>
+        <Link className="card" href="/timeline">
+          Timeline
+        </Link>
 
-<Link className="card" href="/pricing">
-Pricing
-</Link>
+        <Link className="card" href="/pricing">
+          Pricing
+        </Link>
 
-<Link className="card" href="/topics">
-Topics
-</Link>
+        <Link className="card" href="/topics">
+          Topics
+        </Link>
 
-<Link className="card" href="/goals">
-Goals
-</Link>
+        <Link className="card" href="/goals">
+          Goals
+        </Link>
 
-</div>
+      </div>
 
-
-</div>
-)
+    </div>
+  );
 
 }
