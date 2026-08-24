97f798f6dd186ceb6390df1e82a0c5a6e3e3c73c
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-24 16:21:55 -0700

    Add Sophia profile and roles

--- app/people/sophia/page.tsx
+++ app/people/sophia/page.tsx
@@ -1,23 +1,41 @@
-export default function Sophia(){
+export default function Sophia() {
+  return (
+    <div className="peopleBackground">
 
-return(
+      <div className="peopleBox">
 
-<div className="peopleBackground">
+        <h1>
+          Sophia
+        </h1>
 
-<div className="peopleBox">
+        <p>
+          Hey, my name is Sophia!
+          <br /><br />
+          I’m 14 years old and starting my freshman year of high school.
+          <br /><br />
+          I love traveling with my family, listening to music, playing
+          flag football, and spending quality time with my friends. My
+          cousins and I are so excited to share what we’ve created with
+          you all, pouring our blood, sweat, and tears into something we
+          hope you as readers are able to find joy and entertainment in.
+        </p>
 
-<h1>
-Hi I'm Sophia
-</h1>
+        <div className="rolesBox">
 
-<p>
-Coming soon...
-</p>
+          <h2>
+            Roles
+          </h2>
 
-</div>
+          <ul>
+            <li>Copy editor</li>
+            <li>Formatter</li>
+            <li>Author</li>
+          </ul>
 
-</div>
+        </div>
 
-)
+      </div>
 
+    </div>
+  );
 }
