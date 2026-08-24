6e375d19c129c0411db60c05def7475c804a32d5
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-24 16:12:01 -0700

    Add About page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,19 +1,42 @@
-export default function About(){
+export default function About() {
+  return (
+    <div className="background">
 
-return(
+      <div className="pageBox">
 
-<div className="background">
+        <h1>About Us</h1>
 
-<div className="title">
-About
-</div>
+        <div className="aboutText">
 
-<div className="subtitle">
-Coming soon...
-</div>
+          <p>
+            Hi there and welcome to Seemz Like! We are a group of eight
+            cousins who started this magazine as a fun project to share what
+            we find interesting with other people.
+          </p>
 
-</div>
+          <p>
+            Our articles are written by a range of ages starting from 10 to
+            16 years old. We write about all kinds of topics like science,
+            animals, sports, and even tips for school. Every issue also
+            includes fun activities and puzzles. We all have different
+            interests, so there’s something for everyone in our issues!
+          </p>
 
-)
+          <p>
+            We hope our magazine helps you discover something new, keeps you
+            curious, and maybe even find a new favorite topic. We have so
+            much fun collaborating and making these magazines, and we hope
+            you have just as much fun reading it.
+          </p>
 
+          <p>
+            Thanks for checking out our magazine—we’re so happy you’re here!
+          </p>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
 }
