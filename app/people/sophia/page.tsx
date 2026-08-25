7e50a2d96480dfa1d8ddcb847482cb5327552dbd
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-25 16:06:36 -0700

    Sophie about

--- app/people/sophia/page.tsx
+++ app/people/sophia/page.tsx
@@ -9,15 +9,17 @@ export default function Sophia() {
         </h1>
 
         <p>
-          Hey, my name is Sophia!
+      
           <br /><br />
-          I’m 14 years old and starting my freshman year of high school.
+        
           <br /><br />
-          I love traveling with my family, listening to music, playing
-          flag football, and spending quality time with my friends. My
-          cousins and I are so excited to share what we’ve created with
-          you all, pouring our blood, sweat, and tears into something we
-          hope you as readers are able to find joy and entertainment in.
+          Hey, my name is Sophia! I’m 14 years old and starting my 
+          freshman year of high school. I love traveling with my family, 
+          listening to music, playing flag football, and spending quality
+          time with my friends. My cousins and I are so excited to share
+          what we’ve created with you all, pouring our blood, sweat, and 
+          tears into something we hope you as readers are able to find 
+          joy and entertainment in.
         </p>
 
         <div className="rolesBox">
