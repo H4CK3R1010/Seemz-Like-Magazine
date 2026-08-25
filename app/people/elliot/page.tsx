d8482abdfcebe92bd4e3fc8272cf57c8b826e2c0
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-25 16:02:24 -0700

    Add Elliot profile and roles

--- app/people/elliot/page.tsx
+++ app/people/elliot/page.tsx
@@ -1,23 +1,32 @@
-export default function Elliot(){
+export default function Elliot() {
+  return (
+    <div className="peopleBackground">
 
-return(
+      <div className="peopleBox">
 
-<div className="peopleBackground">
+        <h1>
+          Elliot
+        </h1>
 
-<div className="peopleBox">
+        <p>
+          I’m Elliot and I’m in 5th grade and I am 10 years old. One
+          of my hobbies is playing video games such as Roblox, Brawl
+          Stars, FC 26, and Minecraft. I also like traveling to new
+          places like Japan or Italy. I have one older brother and a
+          Corgi. I also like hanging with my cousins. Lastly, I like
+          playing soccer.
+        </p>
 
-<h1>
-Hi I'm Elliot
-</h1>
+        <div className="rolesBox">
 
-<p>
-Coming soon...
-</p>
+          <p>
+            Illustrator, Author
+          </p>
 
-</div>
+        </div>
 
-</div>
+      </div>
 
-)
-
+    </div>
+  );
 }
