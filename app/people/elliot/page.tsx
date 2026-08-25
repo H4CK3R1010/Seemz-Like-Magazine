b13c79eae589876c243fc705aca693bde71556b5
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-25 16:07:52 -0700

    Elliot description

--- app/people/elliot/page.tsx
+++ app/people/elliot/page.tsx
@@ -9,7 +9,7 @@ export default function Elliot() {
         </h1>
 
         <p>
-          I’m Elliot and I’m in 5th grade and I am 10 years old. One
+          Hi, I’m Elliot and I’m in 5th grade and I am 10 years old. One
           of my hobbies is playing video games such as Roblox, Brawl
           Stars, FC 26, and Minecraft. I also like traveling to new
           places like Japan or Italy. I have one older brother and a
