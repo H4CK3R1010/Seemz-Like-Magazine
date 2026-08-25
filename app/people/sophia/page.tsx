6faedecea47b27c7db8b5b7f7c2ac5e30bb1721a
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-25 16:24:52 -0700

    Sophie formatting page

--- app/people/sophia/page.tsx
+++ app/people/sophia/page.tsx
@@ -9,16 +9,12 @@ export default function Sophia() {
         </h1>
 
         <p>
-      
-          <br /><br />
-        
-          <br /><br />
-          Hey, my name is Sophia! I’m 14 years old and starting my 
-          freshman year of high school. I love traveling with my family, 
+          Hey, my name is Sophia! I’m 14 years old and starting my
+          freshman year of high school. I love traveling with my family,
           listening to music, playing flag football, and spending quality
           time with my friends. My cousins and I are so excited to share
-          what we’ve created with you all, pouring our blood, sweat, and 
-          tears into something we hope you as readers are able to find 
+          what we’ve created with you all, pouring our blood, sweat, and
+          tears into something we hope you as readers are able to find
           joy and entertainment in.
         </p>
 
