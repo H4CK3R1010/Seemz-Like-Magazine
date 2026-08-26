ce26969ef9fda11c845e81ce3334a647e12a966a
H4CK3R1010 <zakarymyles@gmail.com>
2026-08-25 20:12:05 -0700

    Enlarge embedded contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,11 +9,14 @@ export default function Contact() {
           <a href="mailto:seemzlike@gmail.com">seemzlike@gmail.com</a>
         </p>
 
+        <p className="contactFormIntro">
+          Or by filling out the form below!
+        </p>
+
         <iframe
           className="googleContactForm"
           src="https://docs.google.com/forms/d/e/1FAIpQLScmmOxsAbXt5wjCZM14ZlZ5rKVhT3ypvQQnZO8sTn2v1HxuQQ/viewform?embedded=true"
           title="Contact Seemz Like Magazine"
-          loading="lazy"
         >
           Loading…
         </iframe>
